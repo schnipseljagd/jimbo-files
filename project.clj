330aff0084ccacfc42d8d8bf7e3a6e816b94811a
Joscha <joscha@schnipseljagd.org>
2013-08-03 02:01:01 +0200

    some resizing

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject jimbo-files "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [clj-aws-s3 "0.3.6"]])
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [clj-aws-s3 "0.3.6"]
+                 [image-resizer "0.1.5"]])
--- src/jimbo_files/core.clj
+++ src/jimbo_files/core.clj
@@ -2,12 +2,19 @@
 
 (require '[aws.sdk.s3 :as s3])
 (require '[clojure.java.io :as io])
+(require '[image-resizer.core :refer :all])
+(require '[image-resizer.format :as format])
 
-(def cred {:access-key "AKIAIUCPDYCDBFXFMNTA", :secret-key "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"})
+(def cred {:access-key "AKIAIUCPDYCDBFXFMNTA", 
+           :secret-key "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"})
 
 (defn print-meta [o]
   println (:metadata o))
 
 (print-meta (s3/get-object cred "jimbo-files" "simple.jpg"))
 
-(io/copy (:content (s3/get-object cred "jimbo-files" "simple.jpg")) (io/output-stream "simple.jpg"))
+(io/copy (:content (s3/get-object cred "jimbo-files" "simple.jpg"))
+         (io/output-stream "simple.jpg"))
+
+(format/as-file (resize (io/file "simple.jpg") 10 10)
+                                  "/tmp/simple_thumb.jpg")
