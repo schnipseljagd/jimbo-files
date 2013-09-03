fade31d32e7f73aad0a8d6c1588cd110832231c6
Joscha <joscha@schnipseljagd.org>
2013-09-03 14:31:38 +0200

    do download from s3 and resizing in one step

--- src/jimbo_files/core.clj
+++ src/jimbo_files/core.clj
@@ -1,20 +1,33 @@
 (ns jimbo-files.core)
 
-(require '[aws.sdk.s3 :as s3])
-(require '[clojure.java.io :as io])
-(require '[image-resizer.core :refer :all])
-(require '[image-resizer.format :as format])
+(require '[aws.sdk.s3 :as s3],
+         '[clojure.java.io :as io],
+         '[image-resizer.core :refer :all],
+         '[image-resizer.format :as format])
 
-(def cred {:access-key "AKIAIUCPDYCDBFXFMNTA", 
-           :secret-key "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"})
+(def s3-cred {:access-key "AKIAIUCPDYCDBFXFMNTA",
+              :secret-key "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"})
 
 (defn print-meta [o]
   println (:metadata o))
 
-(print-meta (s3/get-object cred "jimbo-files" "simple.jpg"))
+(defn copy-s3-object [output]
+  (io/copy (:content (s3/get-object s3-cred "jimbo-files" "simple.jpg"))
+           output))
 
-(io/copy (:content (s3/get-object cred "jimbo-files" "simple.jpg"))
-         (io/output-stream "simple.jpg"))
+(defn resize-and-save [input]
+  (format/as-file (resize input 10 10)
+                  "/tmp/simple_thumb.jpg"))
 
-(format/as-file (resize (io/file "simple.jpg") 10 10)
-                                  "/tmp/simple_thumb.jpg")
+(print-meta (s3/get-object s3-cred "jimbo-files" "simple.jpg"))
+
+(copy-s3-object (io/output-stream "simple.jpg"))
+
+(resize-and-save (io/file "simple.jpg"))
+
+(defn resize-jimbo-image [filename]
+  (with-open [input (:content (s3/get-object s3-cred "jimbo-files" filename))]
+    (resize input 10 10)))
+
+(format/as-file (resize-jimbo-image "simple.jpg")
+                "/tmp/simple_thumb.jpg")
