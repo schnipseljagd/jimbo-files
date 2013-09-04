628faa357a71a8b5eb08ed7cd84800aa50db1961
Joscha <joscha@schnipseljagd.org>
2013-09-04 19:01:57 +0200

    dont resize if image is orig

--- src/jimbo_files/core.clj
+++ src/jimbo_files/core.clj
@@ -1,9 +1,10 @@
 (ns jimbo-files.core)
 
-(require '[aws.sdk.s3 :as s3],
-         '[clojure.java.io :as io],
-         '[image-resizer.core :refer :all],
-         '[image-resizer.format :as format])
+(require '[aws.sdk.s3 :as s3]
+         '[clojure.java.io :as io]
+         '[image-resizer.core :refer :all]
+         '[image-resizer.format :as format]
+         '[image-resizer.util :as util])
 
 (def s3-cred {:access-key (get (System/getenv) "AWS_S3_ACCESS_KEY")
               :secret-key (get (System/getenv) "AWS_S3_SECRET_KEY")})
@@ -17,7 +18,19 @@
   (io/copy (:content (s3/get-object s3-cred s3-bucket path))
            output))
 
+(defn s3-get-object-content [path]
+  (:content (s3/get-object s3-cred s3-bucket path)))
+
 (defn resize-jimbo-image [path width height]
-  (with-open [input (:content (s3/get-object s3-cred s3-bucket path))]
+  (with-open [input (s3-get-object-content path)]
     (resize input width height)))
+
+(defn s3-image-path [website-id image-id]
+  (format "%s/image/%s" website-id image-id))
+
+(defn jimbo-image-as-stream [website-id image-id]
+  (format/as-stream (util/buffered-image (s3-get-object-content (s3-image-path website-id image-id)) ) "jpg"))
+
+(defn resize-jimbo-image-as-stream [website-id image-id type]
+  (format/as-stream (resize-jimbo-image (s3-image-path website-id image-id) 40 60) "jpg"))
 
--- src/jimbo_files/handler.clj
+++ src/jimbo_files/handler.clj
@@ -5,16 +5,14 @@
   (:require [compojure.handler :as handler]
             [compojure.route :as route]))
 
-(defn resize-jimbo-image-as-stream [path type]
-  (format/as-stream (resize-jimbo-image path 40 60) "jpg"))
-
 (defroutes app-routes
   (GET "/" [] "Hello World")
   (GET ["/websites/:website-id/images/:image-id" :website-id #"[0-9]+" :image-id #"[0-9]+"]
-       [website-id image-id & params]
-    (resize-jimbo-image-as-stream 
-      (format "%s/image/%s" website-id image-id)
-      (if (:type params) (:type params) "std")))
+       [website-id image-id]
+    (jimbo-image-as-stream website-id image-id))
+  (GET ["/websites/:website-id/images/:image-id/:type" :website-id #"[0-9]+" :image-id #"[0-9]+"]
+       [website-id image-id type]
+    (resize-jimbo-image-as-stream website-id image-id type))
   (route/not-found "Image not found"))
 
 (def app
