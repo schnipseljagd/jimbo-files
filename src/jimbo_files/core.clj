f477d406dead67bfd2434e6bf2885c9157f3ae70
Joscha <joscha@schnipseljagd.org>
2013-09-10 11:03:04 +0200

    move logic into core again

--- src/jimbo_files/core.clj
+++ src/jimbo_files/core.clj
@@ -1,5 +1,9 @@
 (ns jimbo-files.core
-  (:require [amazonica.aws.s3 :as s3]))
+  (:require [amazonica.aws.s3 :as s3]
+            [clj-jwt.core :refer :all]
+            [image-resizer.core :refer :all]
+            [image-resizer.util :as util]
+            [image-resizer.format :as format]))
 
 (def s3-cred {:access-key (get (System/getenv) "AWS_S3_ACCESS_KEY")
               :secret-key (get (System/getenv) "AWS_S3_SECRET_KEY")})
@@ -11,3 +15,24 @@
 
 (defn s3-image-path [website-id image-id]
   (format "%s/image/%s" website-id image-id))
+
+(defn token->image-data [token]
+  (let [claims (:claims (str->jwt token))]
+    {:website-id (:wid claims) :image-id (:iid claims) :width (:iw claims) :height (:ih claims) :type (:ty claims) :content-type (:ict claims)}))
+
+(defn get-image-as-stream [path resize-algorithm image-data]
+  (let [object (s3-get-object path)]
+    (format/as-stream-by-mime-type (resize-algorithm (:object-content object) (:width image-data) (:height image-data)) (:content-type image-data))))
+
+(defn no-resize [input width height]
+  (util/buffered-image input))
+
+(defn get-image-resize-algorithm [image-type]
+  (case image-type
+    1 resize
+    2 resize-and-crop
+    no-resize))
+
+(defn get-image-as-stream-by-token [token]
+  (let [image-data (token->image-data token)]
+    (get-image-as-stream (s3-image-path (:website-id image-data) (:image-id image-data)) (get-image-resize-algorithm (:type image-data)) image-data)))
--- src/jimbo_files/handler.clj
+++ src/jimbo_files/handler.clj
@@ -4,32 +4,7 @@
         ring.util.response)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
-            [ring.middleware.json :as middleware]
-            [clj-jwt.core :refer :all]
-            [image-resizer.core :refer :all]
-            [image-resizer.util :as util]
-            [image-resizer.format :as format]))
-
-(defn token->image-data [token]
-  (let [claims (:claims (str->jwt token))]
-    {:website-id (:wid claims) :image-id (:iid claims) :width (:iw claims) :height (:ih claims) :type (:ty claims) :content-type (:ict claims)}))
-
-(defn get-image-as-stream [path resize-algorithm image-data]
-  (let [object (s3-get-object path)]
-    (format/as-stream-by-mime-type (resize-algorithm (:object-content object) (:width image-data) (:height image-data)) (:content-type image-data))))
-
-(defn no-resize [input width height]
-  (util/buffered-image input))
-
-(defn get-image-resize-algorithm [image-data]
-  (case (:type image-data)
-    1 resize
-    2 resize-and-crop
-    no-resize))
-
-(defn get-image-as-stream-by-token [token]
-  (let [image-data (token->image-data token)]
-    (get-image-as-stream (s3-image-path (:website-id image-data) (:image-id image-data)) (get-image-resize-algorithm image-data) image-data)))
+            [ring.middleware.json :as middleware]))
 
 (defroutes app-routes
   (GET "/images/:token" [token]
